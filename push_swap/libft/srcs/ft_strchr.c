ace22fe33bbff645bd6e707f8c88092122eee9ad
JVlatt <matthieu.manuellan@gmail.com>
2024-12-27 15:40:29 +0100

    Norm Done

--- push_swap/libft/srcs/ft_strchr.c
+++ push_swap/libft/srcs/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mmanuell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:08:27 by mmanuell          #+#    #+#             */
-/*   Updated: 2024/12/27 14:42:16 by mmanuell         ###   ########.fr       */
+/*   Updated: 2024/12/27 15:40:16 by mmanuell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_strchr(const char *str, int c)
 {
 	unsigned char	uc;
-	
+
 	if (!str)
 		return (NULL);
 	uc = (unsigned char)c;
