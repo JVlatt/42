b0310a8ac0231e690aaa81b80ed4c2c171a88d5c
JVlatt <matthieu.manuellan@gmail.com>
2025-10-08 18:29:51 +0200

    Python Piscine Day 05

---
+++ post-common-core/data/python-4/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Create virtual environment in venv folder
+python -m venv .env
+./.env/bin/pip install --upgrade pip
+./.env/bin/pip install flake8
+
+echo "✅ Virtual environment setup complete!"
+echo "To activate it in the future, run: source venv/bin/activate"
